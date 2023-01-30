3edbe2bef504363ff134c3f88a1b613fb85f1aed
Aryan12360 <123496430+Aryan12360@users.noreply.github.com>
2023-01-30 17:28:36 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var weight=[ 7,15,34,23,43,11,29,37,50,46,33];
+var sum=weight[0]+weight[1]+weight[2]+weight[3]+weight[4]+weight[5]+weight[6]+weight[7]+weight[8]+weight[9]+weight[10];
+var average=sum/weight.length;
+
+console.log(average);
+
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
